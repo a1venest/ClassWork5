129d307404fda109a5939d8b24f5f2621cbce411
Aivenest <aivenest@gmail.com>
2022-10-15 17:26:07 +0500

    add app3

---
+++ app3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 40: Напишите программу, которая
+// принимает на вход три числа и проверяет, может
+// ли существовать треугольник с сторонами такой
+// длины.
+// Теорема о неравенстве треугольника: каждая
+// сторона треугольника меньше суммы двух других
+// сторон.
+
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите число: {i + 1}");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void Triangle(int[] arr)
+{
+    int i = 0;
+    if(arr[i]<arr[i+1]+arr[i+2]&&arr[i+1]<arr[i]+arr[i+2]&&arr[i+2]<arr[i+1]+arr[i])
+    {
+        Console.WriteLine($"Треугольник со сторонами: {arr[i]}; {arr[i+1]}; {arr[i+2]} может существовать.");
+    }
+    else
+    {
+Console.WriteLine($"Треугольник со сторонами: {arr[i]}; {arr[i+1]}; {arr[i+2]} не может существовать.");
+    }
+}
+
+int[] array = new int[3];
+FillArray(array);
+Triangle(array);
