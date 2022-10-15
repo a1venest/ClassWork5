464d1dffe30546222813523579d1e3cd50f967ec
Aivenest <aivenest@gmail.com>
2022-10-15 17:25:48 +0500

    add app2

---
+++ app2/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 10);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] SwapArray(int[] arr)
+{
+    int[] tempArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        tempArr[arr.Length -1 - i] = arr[i];
+    }
+    return tempArr;
+}
+
+void SwapArrayTwo(int[] arr)
+{
+    int swap;
+    for(int i=0; i<arr.Length/2;i++)
+    {
+        swap=arr[arr.Length-i-1];
+        arr[arr.Length-i-1]=arr[i];
+        arr[i]=swap;
+    }
+}
+int[] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
+
+PrintArray(SwapArray(array));
+Console.WriteLine();
+SwapArrayTwo(array);
+PrintArray(array);
