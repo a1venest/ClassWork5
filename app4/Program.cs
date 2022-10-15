5fc8540a278c7bc1258f83be887a588cf74e2e53
Aivenest <aivenest@gmail.com>
2022-10-15 17:26:25 +0500

    add app4

---
+++ app4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string BinarySystem = Convert.ToString(number,2);
+
+Console.WriteLine($"Число {number} в бинарном виде равняется: {BinarySystem}");
