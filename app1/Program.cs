350e791d0d614716566022c7a75ee225fd437a00
Aivenest <aivenest@gmail.com>
2022-10-15 17:25:31 +0500

    add app1

---
+++ app1/Program.cs
@@ -0,0 +1,11 @@
+﻿// int a = 5;
+// int b = 2;
+
+
+// Console.WriteLine($"Переменные: a = {a}, b = {b}");
+
+double number = new Random().NextDouble();
+double number1 = new Random().NextDouble() * 100;
+double number2 = new Random().NextDouble() * 10000;
+
+Console.WriteLine($"Numbers: {Math.Round(number,2)}; {number1}; {number2}");
